abd7f0c39036112a56d4d778c25e7d7173a4adcd
Mdabdul00 <abdulhaqmohammad143@gmail.com>
2025-12-16 15:24:14 +0530

    Update script.js

--- week 8/homepage/script.js
+++ week 8/homepage/script.js
@@ -1,1 +1,4 @@
-
+document.getElementById("welcomeBtn").addEventListener("click", function () {
+    document.getElementById("output").textContent =
+        "Welcome to my CS50 Homepage! Thanks for visiting 😊";
+});
